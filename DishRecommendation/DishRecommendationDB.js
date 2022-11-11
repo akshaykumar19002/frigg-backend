794c45403f49e9990ff16f5587a5f299197509ef
vishalkumar1301 <vishalkumar1301@gmail.com>
2022-11-11 17:52:42 -0500

    fixed bug

--- DishRecommendation/DishRecommendationDB.js
+++ DishRecommendation/DishRecommendationDB.js
@@ -1,41 +1,106 @@
 const db = require('../Config/db');
 const sequelize = require('sequelize');
+// let fridgeList =  sequelize.define('fridge_list', {
+//     id: {
+//         type: DataTypes.INTEGER,
+//         primaryKey: true,
+//         autoIncrement: true
+//     },
+//     fridge_id: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false
+//     },
+//     food_item_id: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false,
+//     },
+//     quantity: {
+//         type: DataTypes.DECIMAL,
+//         allowNull: false
+//     },
+//     purchase_date: {
+//         type: DataTypes.DATE,
+//         allowNull: true
+//     },
+//     expected_expiry_date: {
+//         type: DataTypes.DATE,
+//         allowNull: true
+//     }
+// }, {
+//     underscored: true,
+//     paranoid: true,
+// });
 
-async function GetFoodItemsInRecipe(fridgeId) {
+// const recipe = sequelize.define('recipe_lists', {
+//     id: {
+//         type: DataTypes.INTEGER,
+//         primaryKey: true,
+//         autoIncrement: true
+//     },
+//     recipe_name: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         unique: true
+//     },
+//     ingredients: {
+//         type: DataTypes.STRING
+//     },
+//     type: {
+//         type: DataTypes.STRING
+//     },
+//     calorie_count: {
+//         type: DataTypes.INTEGER
+//     },
+//     is_healthy: {
+//         type: DataTypes.BOOLEAN
+//     }
+// }, {
+//     paranoid: true,
+//     underscored: true
+// });
+// const FoodItem = sequelize.define('food_item', {
+//     id: {
+//         type: DataTypes.INTEGER,
+//         primaryKey: true,
+//         autoIncrement: true
+//     },
+//     name: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//         unique: true
+//     },
+//     expected_expiry_days: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false
+//     }
+// }, {
+//     paranoid: true,
+//     underscored: true
+// });
+
+async function GetFoodItemsInFridge(fridgeId) {
     try {
-        const fridgeList = await db.fridge_list.findAll({
-            where: {
-                fridge_id: fridgeId
-            },
-            include: [{
-                model: db.food_item,
-                attributes: ['name']
-            }],
-            order: [
-                ['expected_expiry_date', 'ASC']
-            ]
-        }); 
-
-        let recipeList = [];
-        for (let i = 0; i < fridgeList.length; i++) {
-            const recipe = await db.recipe_lists.findAll({
-                where: {
-                    ingredients: {
-                        [sequelize.Op.like]: '%' + fridgeList[i].food_item.name + '%'
-                    }
-                }
+        let sql = "SELECT distinct(fi.name) FROM frigg.fridge_lists as fl inner join frigg.food_items as fi on fl.food_item_id = fi.id where fridge_id = " + fridgeId;
+        let food_names = await db.sequelize.query(sql, {
+            type: sequelize.QueryTypes.SELECT
+        });
+        
+        let recepie_list_names = [];
+        for (let i = 0; i < food_names.length; i++) {
+            const element = food_names[i];
+            let sql2 = "select * from recipes where ingredients like '%" + element.name + "%'";
+            let recepie_list = await db.sequelize.query(sql2, {
+                type: sequelize.QueryTypes.SELECT
             });
-            if (recipe.length > 0) {
-                recipeList.push({food_item_name: fridgeList[i].food_item.name, food_item_expiryDate: fridgeList[i].expected_expiry_date});
-            };
-        };
-        return recipeList;
-
+            recepie_list_names.push(...recepie_list.map(x => x.recipe_name));
+        }
+        
+        return recepie_list_names.filter((v, i, a) => a.indexOf(v) === i);
     } catch (error) {
-        throw error;
-    };
+        console.log(error);
+    }
 };
 
 module.exports = {
-    GetFoodItemsInRecipe
+    GetFoodItemsInFridge
 };
